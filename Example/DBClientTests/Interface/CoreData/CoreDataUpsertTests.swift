6af6e8bf4360c63a5da3d2b77b3021f8b4227cd6
Amaranese <32201849+Amaranese@users.noreply.github.com>
2022-01-24 11:49:48 +0100

    Update CoreDataUpsertTests.swift

--- Example/DBClientTests/Interface/CoreData/CoreDataUpsertTests.swift
+++ Example/DBClientTests/Interface/CoreData/CoreDataUpsertTests.swift
@@ -1,10 +1,4 @@
-//
-//  CoreDataUpsertTests.swift
-//  DBClient-Example
-//
-//  Created by Roman Kyrylenko on 2/15/17.
-//  Copyright © 2017 Yalantis. All rights reserved.
-//
+
 
 import XCTest
 @testable import Example
