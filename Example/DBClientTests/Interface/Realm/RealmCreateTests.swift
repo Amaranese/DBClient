a85741e8410f91b2c017b168480b76483a7d28f9
Amaranese <32201849+Amaranese@users.noreply.github.com>
2022-01-24 11:50:41 +0100

    Update RealmCreateTests.swift

--- Example/DBClientTests/Interface/Realm/RealmCreateTests.swift
+++ Example/DBClientTests/Interface/Realm/RealmCreateTests.swift
@@ -1,10 +1,4 @@
-//
-//  RealmCreateTests.swift
-//  DBClient-Example
-//
-//  Created by Roman Kyrylenko on 2/8/17.
-//  Copyright © 2017 Yalantis. All rights reserved.
-//
+
 
 import XCTest
 @testable import Example
