94fd1e3efdeb3107f3d2a795a081d8360ad48b6e
Amaranese <32201849+Amaranese@users.noreply.github.com>
2022-01-24 11:49:57 +0100

    Update DBClientCoreDataTest.swift

--- Example/DBClientTests/Interface/CoreData/DBClientCoreDataTest.swift
+++ Example/DBClientTests/Interface/CoreData/DBClientCoreDataTest.swift
@@ -1,10 +1,4 @@
-//
-//  DBClientCoreDataTest.swift
-//  DBClientTests
-//
-//  Created by Roman Kyrylenko on 10/23/18.
-//  Copyright © 2018 Yalantis. All rights reserved.
-//
+
 
 import XCTest
 import DBClient
