082a0dd85831250a1d0949ad0f12f66bf7abacff
Amaranese <32201849+Amaranese@users.noreply.github.com>
2022-01-24 11:49:31 +0100

    Update CoreDataObservableTests.swift

--- Example/DBClientTests/Interface/CoreData/CoreDataObservableTests.swift
+++ Example/DBClientTests/Interface/CoreData/CoreDataObservableTests.swift
@@ -1,10 +1,4 @@
-//
-//  CoreDataObservableTests.swift
-//  DBClient-Example
-//
-//  Created by Roman Kyrylenko on 2/13/17.
-//  Copyright © 2017 Yalantis. All rights reserved.
-//
+
 
 import XCTest
 import DBClient
