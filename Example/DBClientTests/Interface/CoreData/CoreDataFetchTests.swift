88ddc5f4659addbf4dcd44ce5cfa9b7c6c3d7ffa
Amaranese <32201849+Amaranese@users.noreply.github.com>
2022-01-24 11:49:00 +0100

    Update CoreDataFetchTests.swift

--- Example/DBClientTests/Interface/CoreData/CoreDataFetchTests.swift
+++ Example/DBClientTests/Interface/CoreData/CoreDataFetchTests.swift
@@ -1,10 +1,4 @@
-//
-//  CoreDataFetchTests.swift
-//  DBClient-Example
-//
-//  Created by Roman Kyrylenko on 2/8/17.
-//  Copyright © 2017 Yalantis. All rights reserved.
-//
+
 
 import Foundation
 import XCTest
